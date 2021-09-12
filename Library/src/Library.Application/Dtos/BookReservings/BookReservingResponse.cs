d6e93c063160332344c051f8d146dc3b8f1efc14
bozin96 <nikolabozinovic1996@yahoo.com>
2021-09-12 14:35:43 +0200

    feat(reservation): added reservation models

---
+++ Library/src/Library.Application/Dtos/BookReservings/BookReservingResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Application.Dtos.BookLendings
+{
+    public class BookReservingResponse : BaseResponse
+    {
+        public BookReservingResponse()
+        {
+
+        }
+
+        public BookReservingResponse(bool status, int statusCode, List<string> errors, object data) : base(status, statusCode, errors, data)
+        {
+        }
+    }
+}
